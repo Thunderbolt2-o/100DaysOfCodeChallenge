47f5f38e1ea86abf80c15d786a513a57ffc60e71
Thunderbolt2-o <68735588+Thunderbolt2-o@users.noreply.github.com>
2020-12-03 06:15:31 +0530

    Create Day-1.py
    This day I have learned about the print, input, and len functions and string concatenation

---
+++ Day-1.py
@@ -0,0 +1,65 @@
+# Yeah this is my first line in phython course pretty exited LOL 😂😉
+print("Hello world!")
+# Input given is a string
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+# \n is used to print in a new line in the same print statement 
+print("Hello world!\nHello world!\nHello world!")
+# string concatination in this way we can add two strings 😎
+print ("100Days" + " of " + "CodeChallenge")
+#we should not start with a space we may get indentationError
+#small debugging exercise 
+# Question ^_~
+# print("Day 1 - String Manipulation")
+# #String Concatenation is done with the "+" sign.
+# print("String Concatenation is done with the " + "+"+ " sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+# Answer ^3^
+print("Day 1 - String Manipulation")
+#String Concatenation is done with the "+" sign.
+print("String Concatenation is done with the " + "+"+ " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+# taking input from the user can be taken with the help of "input" function
+# input takes input from the user
+print(input("what is your name?") + " Welcome to 100DaysOfCodeChallenge 😊")
+
+# Coding exercirse print the length of the string
+# reference https://www.geeksforgeeks.org/python-string-length-len/
+# len() function is used to return the size of the  string
+print(len(input("What is your name?")))
+
+# Yay!😋 variables, these are used to store data. It can be changed anytime
+# variables dont start with a number and it should not have no spaces
+challenge = "100DaysOfCode"
+print("Size of the variable challenge is " +len(challenge))
+
+# coding exercise swaping direct solution is pasted here
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+c = 9090 # LOL this line is not requried 
+c = a
+a = b
+b = c
+#Write your code above this line 👆
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+# One of the most important things in coding is, 🚨 code should be always readable 🚨
+
+# Wow😁 first project in python (Beginner😅)
+# solution 👇👇
+# 1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+#2. Ask the user for the city that they grew up in.
+city = input("In which you have grown up?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("what is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name can be" + city +" "+ pet_name)
