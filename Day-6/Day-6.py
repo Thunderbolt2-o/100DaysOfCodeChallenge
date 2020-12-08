e0cbb3a26bf5e15dcf4009381fd1144939f94a70
Thunderbolt2-o <68735588+Thunderbolt2-o@users.noreply.github.com>
2020-12-08 23:22:35 +0530

    Functions
    # today mostly I spent time in exploring the reebrogs's world which an amazing website to learn about the functions and loops and stuff 
    # ref:- https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json

---
+++ Day-6/Day-6.py
@@ -0,0 +1,14 @@
+print("hello")
+a = "hello"
+print(len(a))
+# All the above functions are the examples of the built in functions of the python
+# ref - https://docs.python.org/3/library/functions.html
+
+# How to make our own functions
+# syntax 👇👇
+def my_function(): # defining function  👈👈
+  print("hello")
+  print("Bye")
+
+# function call
+my_function() # 👈👈 
